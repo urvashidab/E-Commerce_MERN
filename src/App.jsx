968eb458a1c33a21e4fdc2871518b7473116c1d0
urvashidab <urvashi.dabgotra11@gmail.com>
2025-11-24 20:12:17 +0530

    all routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Collection from "./pages/Collection";
+import Product from "./pages/Product";
+import About from "./pages/About";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/order" element={<Orders />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
