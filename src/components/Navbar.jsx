8dfbc11787537c469490989ebf612647ae949b56
urvashidab <urvashi.dabgotra11@gmail.com>
2025-11-26 22:35:40 +0530

    Navlink edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,42 @@
 import React from "react";
 import { assets } from "../assets/frontend_assets/assets";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center py-5">
+    <nav className="flex justify-between items-center py-5 font-medium">
       {/* left hand side */}
-      <img src={assets.logo} alt="logo" className="w-40"></img>
+      <img src={assets.logo} alt="logo" className="w-36"></img>
 
       {/* middle part list */}
 
       <ul className="flex justify-center items-center gap-6 ">
-        <li className="">
-          <a href="#">HOME</a>
-        </li>
-        <li>
-          <a href="">COLLCETION</a>
-        </li>
-        <li>
-          <a href="">ABOUT</a>
-        </li>
-        <li>
-          <a href="">CONTACT</a>
-        </li>
-        <li>
-          <a href=""></a>
-        </li>
-        <button>Admin panel</button>
+        <NavLink to="/">
+          <p>HOME</p>
+          <hr></hr>
+        </NavLink>
+
+        <NavLink to="/collection">
+          <p>COLLECTION</p>
+          <hr></hr>
+        </NavLink>
+
+        <NavLink to="/about">
+          <p>ABOUT</p>
+          <hr></hr>
+        </NavLink>
+
+        <NavLink to="/contact">
+          <p>CONTACT</p>
+          <hr></hr>
+        </NavLink>
+
+        <button className="rounded-full shadow py-2 px-4 ">Admin panel</button>
       </ul>
 
       {/* right hand side */}
 
-      <div className="flex justify-center gap-4">
-        <img src={assets.search_icon} alt="search icon"></img>
+      <div className="flex justify-center gap-6">
+        <img src={assets.search_icon} alt="search icon" className=""></img>
         <img src={assets.profile_icon} alt="profile icon"></img>
         <img src={assets.cart_icon} alt="cart icon"></img>
       </div>
