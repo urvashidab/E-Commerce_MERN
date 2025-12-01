0b53bcc5c39bec4ed00c5881f383d275f62db4ed
urvashidab <urvashi.dabgotra11@gmail.com>
2025-12-01 22:03:21 +0530

    Login added

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="flex gap-2 items-center justify-center py-10">
+        <h1 className="uppercase text-2xl text-gray-500 font-medium ">Login</h1>
+        <span className="linedesign"></span>
+      </div>
+    </>
+  );
+};
+
+export default Login;
