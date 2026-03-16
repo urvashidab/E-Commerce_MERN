311f66f6f7061c03423443e775079b1fa959f4e4
urvashidab <urvashi.dabgotra11@gmail.com>
2026-03-16 21:43:52 +0530

    Collection component edited

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,32 +1,84 @@
-import React from "react";
+import React, { useContext } from "react";
 import Line from "../components/Line";
-import { products } from "../assets/frontend_assets/assets";
+import { ProductContext } from "../context/ProductContext";
 import Title from "../components/Title";
+import ProductItems from "../components/ProductItems";
 
 const Collection = () => {
+  const { products } = useContext(ProductContext);
   return (
     <>
       <Line />
       <Title text1="all" text2="collections" className="py-10" />
 
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 cursor-pointer">
-          {products.map((item) => (
-            <div key={item.id} className="flex flex-col  justify-start ">
-              {/* image */}
-              <div className="overflow-hidden rounded">
-                <img
-                  loading="lazy"
-                  className="  hover:scale-105 transition-transform duration-200"
-                  src={item.image}
-                  alt="product image"
-                />
-              </div>
-              <p>{item.name}</p>
-              <p className="font-semibold text-sm">${item.price}</p>
+      {/* whole container starts */}
+      <div className="flex gap-4 justify-between  ">
+        {/* filters start */}
+
+        <div className="w-1/5 flex flex-col gap-4  ">
+          {/* <h1>FILTERS</h1> */}
+
+          {/* first category starts */}
+          <div className="flex flex-col gap-4 justify-start border border-gray-400 rounded py-4 px-6 ">
+            <p>CATEGORIES</p>
+
+            <div className="flex flex-col gap-2 ">
+              <label>
+                <input type="checkbox" />
+                Men
+              </label>
+              <label>
+                <input type="checkbox" />
+                Women
+              </label>
+              <label>
+                <input type="checkbox" />
+                Kids
+              </label>
             </div>
-          ))}
+          </div>
+
+          {/* second category starts */}
+          <div className="flex flex-col gap-4 border border-gray-400 rounded py-4 px-6">
+            <p>TYPE</p>
+
+            <div className="flex flex-col gap-2">
+              <label>
+                <input type="checkbox" />
+                Tops
+              </label>
+              <label>
+                <input type="checkbox" />
+                Bottoms
+              </label>
+              <label>
+                <input type="checkbox" />
+                Winterwear
+              </label>
+            </div>
+          </div>
+
+          {/* second category ends */}
         </div>
+
+        {/* filters end */}
+
+        {/* products items for all the products */}
+        <div className="flex-1">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 cursor-pointer">
+            {products.map((item) => (
+              <ProductItems
+                key={item._id}
+                _id={item._id}
+                name={item.name}
+                price={item.price}
+                image={item.image}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* container ends */}
       </div>
     </>
   );
