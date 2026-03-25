93f03015afc238d959abdec6663968b804731d34
urvashidab <urvashi.dabgotra11@gmail.com>
2026-03-25 20:49:08 +0530

    Reset Password page's style changed

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -6,37 +6,43 @@ import { NavLink } from "react-router-dom";
 const ResetPassword = () => {
   return (
     <>
-      <Line />
+      <div className="bg-black text-white min-h-[70vh] flex flex-col justify-center">
+        <h1 className="text-center text-2xl py-10 tracking-widest font-light">
+          RESET PASSWORD
+        </h1>
+        <div className="max-w-lg px-4 mx-auto ">
+          <p className="text-gray-300 text-sm mb-6">
+            Enter your email address and we will send you instructions to reset
+            your password.
+          </p>
 
-      <Title text1="reset" text2="password" className="py-12" />
+          <form className="flex flex-col gap-4">
+            <input
+              type="email"
+              required
+              placeholder="E-MAIL"
+              className="border-b border-gray-400 py-2 px-2 outline-none placeholder:text-gray-400"
+            />
 
-      <div className="max-w-lg px-4 mx-auto min-h-[50vh] flex flex-col justify-center">
-        <p className="text-gray-500 text-sm mb-6">
-          Enter your email address and we will send you instructions to reset
-          your password.
-        </p>
+            <button
+              type="submit"
+              className="border border-white px-8 py-2.5 tracking-wide
+hover:bg-white hover:text-black hover:tracking-widest cursor-pointer
+transition-all duration-300
+            "
+            >
+              SEND RESET LINK
+            </button>
+          </form>
 
-        <form className="flex flex-col gap-4">
-          <input
-            type="email"
-            required
-            placeholder="E-MAIL"
-            className="border-b border-gray-400 py-2 px-2 outline-none placeholder:text-gray-400"
-          />
-
-          <button
-            type="submit"
-            className=" primaryBtn
-            mt-4"
-          >
-            SEND RESET LINK
-          </button>
-        </form>
-
-        <div className="mt-6 text-sm">
-          <NavLink to="/login" className="underline hover:text-gray-500">
-            Back to Login
-          </NavLink>
+          <div className="mt-6 text-sm">
+            <NavLink
+              to="/login"
+              className="underline text-gray-300 hover:text-gray-500"
+            >
+              Back to Login
+            </NavLink>
+          </div>
         </div>
       </div>
     </>
